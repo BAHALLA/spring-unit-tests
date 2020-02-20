72dbf0f5a4193258942c26f1a121e0761ca3a0c2
bahalla <bahalla.taoufiq@gmail.com>
2020-02-20 15:44:11 +0100

    add index controller

--- src/main/java/org/sid/controllers/EmployeeRestController.java
+++ src/main/java/org/sid/controllers/EmployeeRestController.java
@@ -7,16 +7,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 @RestController
-@RequestMapping("/api")
 public class EmployeeRestController {
     @Autowired
     private EmployeeService employeeService;
 
+    @GetMapping("/")
+    public Map<String, String> index() {
+        Map<String,String> list =new HashMap<>();
+        Stream.of("a","b", "c","d").forEach( p -> {
+            list.put(p, p + "_v");
+        });
+        return list;
+    }
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeService.findAllEmployees();
     }
+
 }
